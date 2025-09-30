61f65385d1a1e44106045424b44d826aa385575a
rolando314 <rar339@cornell.edu>
2025-09-29 21:09:09 -0400

    Initial Commit

---
+++ Chronicler/ChroniclerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChroniclerApp.swift
+//  Chronicler
+//
+//  Created by Rolando on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct ChroniclerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
